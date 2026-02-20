5b93178a738b30eca8cebae97d7fbd9c9587c0bc
Julio Souza <licemaraparecida@gmail.com>
2026-02-20 15:30:51 -0300

    import fix

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,8 +1,10 @@
+import hard_video from "../assets/videos/=).mp4"
+
 function Notfound() {
     return (
         <div className="m-0  w-full h-full overflow-hidden" onClick={() => {document.getElementById("video").muted = false}}>
-            <video src="src\assets\videos\=).mp4" muted autoPlay loop className="bg-video fixed inset-0 w-screen h-screen object-fill -z-10 blur-lg brightness-75"></video>
-            <video id="video" src="src\assets\videos\=).mp4" muted autoPlay loop className="main-video w-[99vw] h-[99vh]"></video>
+            <video src={hard_video} muted autoPlay loop className="bg-video fixed inset-0 w-screen h-screen object-fill -z-10 blur-lg brightness-75"></video>
+            <video id="video" src={hard_video} muted autoPlay loop className="main-video w-[99vw] h-[99vh]"></video>
         </div>
     )
 }
