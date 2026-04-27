2091e1e451153ef6caf381ed8c14c54668d596b5
Julio Souza <licemaraparecida@gmail.com>
2026-04-27 20:43:42 -0300

    modal ok

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -66,12 +66,12 @@ function Modal() {
 
     return (
         <div className="fixed top-16 w-screen h-[calc(100vh-4rem)] overflow-auto flex justify-around items-center mb-8" id="modal_bg">
-            <div className="fixed top-0 w-full h-screen" onClick={() => navigate(`/gallery/${category}`)}></div>
-            <div  className="fixed z-50 flex flex-col items-center justify-center max-w-[90%]" id="modal_div">
-                <div className="relative flex items-center justify-center grow">
+            <div className="fixed top-0 w-full h-screen backdrop-blur-xs bg-[#03071281]" onClick={() => navigate(`/gallery/${category}`)}></div>
 
+            <div  className="fixed z-50 flex flex-col items-center justify-center" id="modal_div">
+                <div className="relative flex flex-row items-center justify-center max-w-5xl pointer-events-auto">
                     <a onClick={() => navigate(`/gallery/${category}`)} className="fixed z-30 rounded-full bg-[rgba(0,0,0,.5)] w-12 h-12 right-2 sm:right-2/12 top-40 flex justify-center items-center select-none"><span className="text-cyan-500 text-4xl">×</span></a>
-                    <a  className="fixed z-30 rounded-full bg-[rgba(0,0,0,.5)] w-12 h-12 left-2 sm:left-2/12 flex justify-center  pl-2 items-center"><i className="arrow select-none w-6 h-6 rotate-135"></i></a>
+                    <a  className="fixed z-30 rounded-full bg-[#000000f0] opacity-35 w-12 h-12 left-2 sm:left-2/12 flex justify-center  pl-2 items-center"><i className="arrow select-none w-6 h-6 rotate-135"></i></a>
                     {piece?.file_type == "image"? 
                         <img id="modal_img" className="max-h-[60vh]" src={piece?.url}/>
                     : 
@@ -83,8 +83,8 @@ function Modal() {
                         }}></button> */}
                         </video>
                     }
-                    {user?.app_metadata?.is_admin? <a className="bg-red-500" onClick={delete_media}>DELETE</a> : null}
-                    <a  className="fixed z-30 rounded-full bg-[rgba(0,0,0,.5)] w-12 h-12 right-2 sm:right-2/12 flex justify-center  pr-2 items-center"><i className="arrow select-none w-6 h-6 -rotate-45"></i></a>
+                    {user?.app_metadata?.is_admin? <a className="absolute bottom-0 left-0 bg-red-700" onClick={delete_media}>DELETE</a> : null}
+                    <a  className="fixed z-30 rounded-full bg-[#000000f0] opacity-35 w-12 h-12 right-2 sm:right-2/12 flex justify-center  pr-2 items-center"><i className="arrow select-none w-6 h-6 -rotate-45"></i></a>
                 </div> 
                 {/* onClick={() => change_modal(1)} */}
                 <h2>{media_name}</h2>
