57993723447568e29b57f08a6df942827cd5578d
Julio Souza <licemaraparecida@gmail.com>
2026-07-16 15:13:19 -0300

    possible to fully edit and delete blog posts

--- src/pages/blog/create-post.jsx
+++ src/pages/blog/create-post.jsx
@@ -12,7 +12,7 @@ import { v4 } from "uuid"
 import star_icon from "../../assets/images/star_icon.png"
 
 function Content_input_div({id, content_value, on_delete, on_change}) {
-    const [ select_value, set_select_value ] = useState(content_value.type? content_value.type :"text")
+    const [ selected_type, set_selected_type ] = useState(content_value.type? content_value.type :"text")
     const [ image, set_image ] = useState(content_value.type === "image"? content_value.value:null)
     const [ audio, set_audio ] = useState(content_value.type === "audio"? content_value.value:null)
     const [ video, set_video ] = useState(content_value.type === "video"? content_value.value:null)
@@ -20,8 +20,12 @@ function Content_input_div({id, content_value, on_delete, on_change}) {
     
     useEffect(() => {
         console.log("CONTEUDO Q CHEGOU:", content_value);
-        
-    }, [])
+        set_selected_type(content_value.type)
+        // if(content_value.type === "image") set_image(content_value.value)
+        // if(content_value.type === "audio") set_audio(content_value.value)
+        // if(content_value.type === "video") set_video(content_value.value)
+        // if(content_value.type === "embed") set_embed(content_value.value)
+    }, [content_value.type, content_value.value])
 
     const update_content = (value, type) => {
         if (type === "text") {
@@ -59,9 +63,9 @@ function Content_input_div({id, content_value, on_delete, on_change}) {
     }
     
     function Render_content() {
-        switch (select_value) {
+        switch (selected_type) {
             case "text":
-                return <textarea id={id} onChange={(e) => update_content(e.target.value, "text")} className="p-0.5 pl-2 max-h-64 min-h-64 w-full bg-gray-950 border border-cyan-600 mb-16 rounded-sm" placeholder="Content"/>
+                return <textarea id={id} value={content_value.value} onChange={(e) => update_content(e.target.value, "text")} className="p-0.5 pl-2 max-h-64 min-h-64 w-full bg-gray-950 border border-cyan-600 mb-16 rounded-sm" placeholder="Content"/>
                 
             case "image":
                 return (
@@ -123,7 +127,7 @@ function Content_input_div({id, content_value, on_delete, on_change}) {
     }
     return (
         <div name={id} className="relative flex flex-row mb-12 w-full h-64">
-            <select name="content_type" className="p-0.5 pl-2 bg-gray-950 border border-cyan-600 rounded-sm max-h-7.5 mr-2" onChange={(e) => set_select_value(e.target.value)}>
+            <select name="content_type" className="p-0.5 pl-2 bg-gray-950 border border-cyan-600 rounded-sm max-h-7.5 mr-2" value={selected_type} onChange={(e) => set_selected_type(e.target.value)}>
                 <option value="text">text</option>
                 <option value="image">img</option>
                 <option value="audio">audio</option>
@@ -139,7 +143,9 @@ function Content_input_div({id, content_value, on_delete, on_change}) {
 
 
 function CreatePost() {
-    const { post_id } = useParams()
+    const { post_id: old_post_id } = useParams()
+    console.log(old_post_id);
+    
     
     const [icon_img, set_icon_img] = useState(null)
     const [thumbnail_img, set_thumbnail_img] = useState(null)
@@ -164,7 +170,6 @@ function CreatePost() {
         getUser()
     }, [])
     
-    useEffect(() => {if(content_list.length === 0 ) add_content_block()}, [])
         
         useEffect(() => {
             const { data: listener } = supabase.auth.onAuthStateChange(
@@ -179,12 +184,12 @@ function CreatePost() {
     
     useEffect(() => {
         async function load_post() {
-            console.log("tem post, id: ", post_id);
+            console.log("tem post, id: ", old_post_id);
 
             const {data: post_data, error} = await supabase
             .from("blog_posts")
             .select("*")
-            .eq("id", post_id)
+            .eq("id", old_post_id)
             .single()
             
             if (error) {
@@ -205,7 +210,10 @@ function CreatePost() {
             set_content_list(loaded_content)
 
         }
-        if (post_id) load_post()
+        
+    
+        if(old_post_id) load_post() 
+        else add_content_block()
     }, [])
 
 
@@ -216,7 +224,10 @@ function CreatePost() {
     async function handle_submit(e) {
         e.preventDefault()
 
-        const post_id = v4()
+        const editing = !!old_post_id
+        const post_id = old_post_id ?? v4()
+        
+        
         let formated_content = []
 
         // read every content, passing just type and value, if its an image, upload to bucket and set bucket publicurl as value
@@ -228,37 +239,38 @@ function CreatePost() {
                 const input = document.getElementById(c.id)
                 const file = input.files[0]
                 const ext = file.name.split('.').pop()
+                if (!input.files?.length)  continue
+                if (input.files.length > 0) {
+                    const file_path = `${post_id}/${c.id}.${ext}`
 
-                if (input.files.length === 0) continue
+                    const {error: media_upload_error} = await supabase.storage
+                    .from("blog_media")
+                    .upload(file_path, file, {upsert: true})
 
-                const file_path = `${post_id}/${c.id}.${ext}`
+                    if (media_upload_error) {
+                        // define error reacion here
+                        console.log("content upload error down here: ");
+                        console.error(media_upload_error);
+                        continue
+                    }
 
-                const {error: img_upload_error} = await supabase.storage
-                .from("blog_media")
-                .upload(file_path, file, {upsert: true})
+                    const {data} = supabase.storage
+                    .from("blog_media")
+                    .getPublicUrl(file_path)
 
-                if (img_upload_error) {
-                    // define error reacion here
-                    console.log("content upload error down here: ");
-                    console.error(img_upload_error);
-                    continue
+                    c.value = data.publicUrl
                 }
-
-                const {data} = supabase.storage
-                .from("blog_media")
-                .getPublicUrl(file_path)
-
-                c.value = data.publicUrl
             }
             formated_content.push({type: c.type, value:c.value})
         }
 
-        console.log("result: ", formated_content);
+        console.log("content to be posted: ", formated_content);
+
 
 
         var thumbnail_public_url
         if (document.getElementById('thumbnail_input').files[0]) {
-            thumbnail_file = document.getElementById('thumbnail_input').files[0]
+            const thumbnail_file = document.getElementById('thumbnail_input').files[0]
             const thumbnail_ext = thumbnail_file.name.split('.').pop()
             const thumbnail_file_path = `${post_id}/thumbnail.${thumbnail_ext}`
             const {error: thumbnail_upload_error} = await supabase.storage
@@ -313,25 +325,43 @@ function CreatePost() {
 
 
 
-        if (post_id) {
+        if (editing) {
             const {data, error: post_submit_error} = await supabase
             .from("blog_posts")
             .update([
                 {
-                    id: post_id,
+                    id: old_post_id,
                     slug: name_input,
                     url_slug: url_name,
                     title: title_input,
                     icon: icon_public_url,
                     thumbnail: thumbnail_public_url,
                     summary: description_input,
-                    content: formated_content
+                    content: formated_content,
                 }
             ])
+            .eq("id", old_post_id)
             if (post_submit_error) {
                 console.error(post_submit_error);
                 return
             }
+            const { data: posts, error } = await supabase
+            .from('blog_posts')
+            .select('*')
+            .eq("id", old_post_id)
+            
+            console.log("| id: ", old_post_id)
+            console.log("| slug: ", name_input)
+            console.log("| url_slug: ", url_name)
+            console.log("| title: ", title_input)
+            console.log("| icon: ", icon_public_url)
+            console.log("| thumbnail: ", thumbnail_public_url)
+            console.log("| summary: ", description_input)
+            console.log("| content: ", formated_content)
+            
+            console.log(posts);
+            
+            
 
         }else {
             const {data, error: post_submit_error} = await supabase
@@ -352,12 +382,46 @@ function CreatePost() {
                 console.error(post_submit_error);
                 return
             }
+            
 
         }
         
         console.log("Post criado!")
+        
+        async function delete_post_files() {
+            const {data: files, error} = await supabase.storage
+            .from("blog_media")
+            .list(post_id)
+            if (error) {
+                console.error(error);
+                return
+            }
+            if (!files.length) return
 
-        navigate("/blog")
+            const used_files_id = new Set ([
+                ...(icon_public_url? ["icon"]: []),
+                ...(thumbnail_public_url? ["thumbnail"]: []),
+                ...content_list.filter(c => c.type !== "text" && c.type !== "embed")
+                .map(c=> `${c.id}`)
+            ])
+            console.log("deletação:");
+            
+            console.log(files);
+            console.log(used_files_id);
+            
+            await supabase.storage
+            .from("blog_media")
+            .remove(
+                files.filter(file => {
+                    const id = file.name.split(".")[0];
+                    return !used_files_id.has(id);
+                })
+                .map(file => `${post_id}/${file.name}`)
+            )
+        }
+        await delete_post_files()
+
+        // navigate("/blog")
         return
     }
 
@@ -395,7 +459,27 @@ function CreatePost() {
         }
         // sett icon here 
     }
-
+    async function delete_post() {
+        await supabase.storage
+        .from("blog_media").remove(
+            ((await supabase.storage
+                .from("blog_media")
+                .list(old_post_id)
+            ).data.map(file => `${old_post_id}/${file.name}`))
+        )
+        await supabase.from("blog_posts")
+        .delete()
+        .eq("id", old_post_id)
+        navigate("/blog")
+    }
+    function remove_icon() {
+        document.getElementById('icon_input').value = null
+        set_icon_img(null)
+    }
+    function remove_thumbnail() {
+        document.getElementById('thumbnail_input').value = null
+        set_thumbnail_img(null)
+    }
     return (
         <Screen>
             <Header/>
@@ -407,10 +491,11 @@ function CreatePost() {
                     <form className="relative w-full max-w-250 bg-gray-900 rounded-lg flex flex-col px-8 py-4 overflow-auto" onSubmit={handle_submit}>
                         <h1 className="font-bold text-4xl text-cyan-600 mb-8">create post</h1>
                         <div className="flex flex-row justify-around gap-4">
+                            {icon_img && <button type="button" onClick={remove_icon} className="flex justify-center items-center right-0 w-8 h-8 bg-amber-950 border border-b-amber-950 border-l-amber-950 border-t border-r hover:border-l-[rgb(83,91,243)] hover:border-b-[rgb(83,91,243)] hover:text-amber-50 rounded-bl-sm rounded-tr-sm transition-all duration-300">X</button>}
 
                             <div className="relative flex justify-center items-center h-7.5 w-7.5 border rounded-sm border-cyan-600 bg-gray-950 hover:border-cyan-900 transition-all duration-300">
                                 <span className="absolute text-lg -translate-y-0.5">+</span>
-                                <img src={icon_img} className="absolute h-full w-full rounded-sm"/>
+                                <img src={icon_img} className={`absolute h-full w-full rounded-sm ${icon_img ? "opacity-100" : "opacity-0"}`}/>
                                 <input type="file" id="icon_input" className="p-0.5 pl-2 w-7.5 bg-gray-950 border border-cyan-600 mb-4 rounded-sm opacity-0" onChange={change_icon_img}/>
                             </div>
 
@@ -422,6 +507,7 @@ function CreatePost() {
                         <div className="relative flex max-w-full min-w-64 w-fit min-h-64 mb-4">
                             <img src={thumbnail_img} className="h-full  rounded-sm"/>
                             <input type="file" id="thumbnail_input" className={`absolute h-64 w-full rounded-sm bg-gray-950 border border-cyan-600 ${thumbnail_img? "opacity-0" : "opacity-100"}`} onChange={change_thumbnail_img}/>
+                            {thumbnail_img && <button type="button" onClick={remove_thumbnail} className="absolute flex justify-center items-center right-0 w-8 h-8 bg-amber-950 border border-b-amber-950 border-l-amber-950 border-t border-r hover:border-l-[rgb(83,91,243)] hover:border-b-[rgb(83,91,243)] hover:text-amber-50 rounded-bl-sm rounded-tr-sm transition-all duration-300">X</button>}
                         </div>
 
                         <textarea type="text" id="description_input" className="p-0.5 pl-2 min-h-32 bg-gray-950 border border-cyan-600 mb-4 rounded-sm" placeholder="summary" value={summary} onChange={(e) => set_summary(e.target.value)}/>
@@ -435,7 +521,8 @@ function CreatePost() {
 
                         <a onClick={add_content_block} className="h-12 w-12 rounded-md font-bold text-3xl flex justify-center items-center pb-1.5 select-none text-cyan-600 bg-gray-950 border border-transparent hover:border-[rgb(83,91,243)] transition-all duration-300">+</a>
 
-                        <button type="submit" className="fixed bottom-12 right-4 bg-gray-950 sm:bottom-14 sm:right-16 border-2 h-8 w-32 p-0 rounded-md border-cyan-950  hover:border-[rgb(83,91,243)] "><span className="w-full h-full select-none text-md flex items-center justify-center text-cyan-600">{post_id? "Edit Post" : "Create Post"}</span></button>
+                        <button type="submit" className="fixed bottom-12 right-4 bg-gray-950 sm:bottom-14 sm:right-16 border-2 h-8 w-32 p-1 rounded-md border-cyan-950  hover:border-[rgb(83,91,243)] "><span className="w-full h-full select-none text-md flex items-center justify-center text-cyan-600 font-semibold">{old_post_id? "Save Post" : "Create Post"}</span></button>
+                        <button type="button" onClick={delete_post} className="absolute top-4  bg-red-600 sm:right-16 border h-8 w-32 p-1 rounded-md border-cyan-950  hover:border-red-400 "><span className="w-full h-full select-none text-md flex items-center justify-center text-red-300 font-semibold">Delete Post</span></button>
                     </form>
 
                 </div>
